f84666e75eac9ff5ffded5687e9ab09bdba21613
Logan McLintock <loganpkg@gmail.com>
2025-12-03 22:55:14 +1100

    start suco project from scratch by writing buf implementation

---
+++ buf.c
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2025 Logan Ryan McLintock. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "buf.h"
+#include "int.h"
+
+struct buf {
+    void *a;                    /* Memory. */
+    size_t i;                   /* Index of next unused element. */
+    size_t n;                   /* Number of elements allocated. */
+    size_t es;                  /* Element size. */
+};
+
+Buf init_buf(size_t init_num_elements, size_t element_size)
+{
+    Buf t;
+
+    if (!init_num_elements || !element_size)
+        return NULL;
+
+    if ((t = calloc(1, sizeof(struct buf))) == NULL)
+        return NULL;
+
+    if (mult_overflow(init_num_elements, element_size)) {
+        free(t);
+        return NULL;
+    }
+
+    if ((t->a = calloc(init_num_elements, element_size)) == NULL) {
+        free(t);
+        return NULL;
+    }
+
+    t->n = init_num_elements;
+    t->es = element_size;
+
+    return t;
+}
+
+void free_buf(Buf b)
+{
+    if (b != NULL) {
+        free(b->a);
+        free(b);
+    }
+}
+
+int push(Buf b, void *object)
+{
+    /* Assumes object is of size b->es. */
+    size_t new_n;
+    void *t;
+
+    if (b->i == b->n) {
+        /* Need to grow the buffer. */
+        if (mult_overflow(b->n, 2))
+            return 1;
+
+        new_n = b->n * 2;
+
+        if (mult_overflow(new_n, b->es))
+            return 1;
+
+        if ((t = realloc(b->a, new_n * b->es)) == NULL)
+            return 1;
+
+        b->a = t;
+        b->n = new_n;
+    }
+
+    memmove((char *) b->a + b->i++ * b->es, object, b->es);
+
+    return 0;
+}
+
+int pop(Buf b, void *result)
+{
+    if (!b->i)
+        return 1;
+
+    memmove(result, (char *) b->a + --b->i * b->es, b->es);
+
+    return 0;
+}
---
+++ build.sh
@@ -0,0 +1,115 @@
+#! /bin/sh
+
+#
+# Copyright (c) 2025 Logan Ryan McLintock. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+set -e
+set -u
+set -x
+
+c_ops='-ansi -g -O0 -Wall -Wextra -pedantic'
+indent_ops='-kr -nut -l80 -T Buf'
+
+
+build_c() {
+    # shellcheck disable=SC2086
+    find . -type f -name '*.h' -exec cc -c $c_ops '{}' \;
+    # shellcheck disable=SC2086
+    find . -type f -name '*.c' -exec cc -c $c_ops '{}' \;
+}
+
+
+wd=$(pwd)
+export wd
+build_dir=$(mktemp -d)
+error_file="$build_dir"/error_file
+export error_file
+
+
+# Copy files to build directory.
+rsync -av --exclude '.git/' . "$build_dir"
+
+cd "$build_dir" || exit 1
+
+find . -type f -name '*.sh' -exec sh -c \
+    'fn="$1"; shellcheck "$1" >> "$error_file"' 'sh' '{}' \;
+
+# Update header files.
+find . -type f -name '*.h' -exec sh -c '
+        set -e
+        set -u
+        set -x
+        fn="$1"
+        line_num=$(grep -n -E "Function declarations" "$fn" | cut -d ":" -f 1)
+        if [ -z "$line_num" ]
+        then
+           printf "%s: ERROR: Function declarations not in header: %s\n" \
+                "$0" "$fn" >> "$error_file"
+
+           exit 1
+        fi
+        head -n "$line_num" "$fn" > "$fn"~
+        c_fn=$(printf %s "$fn" | sed -E "s/\.h$/.c/")
+        {
+            grep -E "^[a-zA-Z][^}{]+$" "$c_fn" | sed -E "s/$/;/"
+            printf "\n%s\n" "#endif"
+        } >> "$fn"~
+        mv "$fn"~ "$fn"
+    ' sh '{}' \;
+
+if [ -s "$error_file" ]
+then
+    cat "$error_file"
+    exit 1
+fi
+
+build_c
+
+# shellcheck disable=SC2086
+find . -type f \( -name '*.h' -o -name '*.c' \) \
+    -exec indent $indent_ops '{}' \;
+
+# Check for long lines.
+if grep -r -I --exclude-dir='.git' -E '.{80}'
+then
+    printf '%s: ERROR: Long lines.\n' "$0"
+    exit 1
+fi
+
+build_c
+
+# Move C files back.
+find . -type f \( -name '*.h' -o -name '*.c' \) -exec sh -c '
+        set -e
+        set -u
+        set -x
+        fn=$(basename "$1")
+        mv "$fn" "$wd/$fn"
+    ' sh '{}' \;
+
+# shellcheck disable=SC2086
+cc $c_ops test_buf.o buf.o int.o -o test_buf
+valgrind ./test_buf
+mv test_buf "$wd"/test_buf
